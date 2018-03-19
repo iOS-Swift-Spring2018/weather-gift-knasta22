f4df27912e4178c81bd8c240cad0184bf7598d04
Kerry Nasta <KerryNasta@kerrys-mbp.bc.edu>
2018-03-18 20:00:05 -0400

    Nasta WeatherGift

---
+++ WeatherGift/ListVC.swift
@@ -0,0 +1,92 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by Kerry Nasta on 3/17/18.
+//  Copyright © 2018 Kerry Nasta. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    
+    var locationsArray = [String]()
+    var currentPage = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToPageVC" {
+            let destination = segue.destination as! PageVC
+            currentPage = (tableView.indexPathForSelectedRow?.row)!
+            destination.currentPage = currentPage
+            destination.locationsArray = locationsArray
+        }
+    }
+    
+    @IBAction func editBarButtonPressed(_ sender: Any) {
+        if tableView.isEditing == true {
+            tableView.setEditing(false, animated: true)
+            editBarButton.title = "Edit"
+            addBarButton.isEnabled = true
+        } else {
+            tableView.setEditing(true, animated: true)
+            editBarButton.title = "Done"
+            addBarButton.isEnabled = false
+        }
+    }
+    
+    
+}
+
+extension ListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        cell.textLabel?.text = locationsArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK:- tableView Editing Functions
+    //actually deleting cells from table view
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            locationsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    //ability to move (rearrange) rows in a table
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = locationsArray[sourceIndexPath.row]
+        locationsArray.remove(at: sourceIndexPath.row)
+        locationsArray.insert(itemToMove, at: destinationIndexPath.row)
+    }
+    
+    //MARK:- tableView methods to freeze the first cell
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        //ternary operator method ("if *blank* is not equal to 0, return turn, else return false)
+        return (indexPath.row != 0 ? true : false)
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        //ternary operator method ("if *blank* is not equal to 0, return turn, else return false)
+        return (indexPath.row != 0 ? true : false)
+    }
+    
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        //ternary operator method
+        return (proposedDestinationIndexPath.row == 0 ? sourceIndexPath : proposedDestinationIndexPath)
+    }
+}
